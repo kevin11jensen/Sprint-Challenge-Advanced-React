c03394a64bf543c54e154d71e1b3fbc7523dadef
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-21 21:15:03 -0800

    re-writing this app from scratch, I lost track of what does what

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import axios from 'axios';
 
-import Navigation from './components/Navigation';
-import InputForm from './components/InputForm';
-import PlayerCard from './components/PlayerCard';
+
 import './App.css';
 
-class App extends Component() {
-  constructor() {
-    super();
-    this.state = {
-      player: [],
-      playerText: ''
-    }
-  }
-
-  componentDidMount() {
-    axios
-    .get(`http://localhost:5000/api/players`)
-    .then(res =>
-      {
-        this.setState({
-          player: res.data
-        })
-        console.log(res)
-      })
-      .catch(err => {
-        console.log('error in player data', err)
-      })
-  }
-
-  handleChanges = e => {
-    this.setState({
-      playerText: e.target.value
-    })
-  }
-
-  fetchPlayer = e => {
-    e.preventDefault()
-    axios
-    .get(`http://localhost:5000/api/${this.state.playerText}`)
-    .then(res => {
-      this.setState({
-        player: res.data
-      })
-      
-    })
-.catch(err => console.log('error in playerText', err))
-  }
-
-  render(){
+const App = () => {
+  
+  
     return (
       <div className="App">
-        <Navigation />
-        <InputForm 
-          updateText={this.state.playerText}
-          changeHandler={this.handleChanges}
-          fetch={this.fetchPlayer}
-        />
-        <PlayerCard
-        player={this.state.player}
-        />
+       <h1>am i even working?</h1>
         </div>
     )
-  }
+  
 }
   
 
