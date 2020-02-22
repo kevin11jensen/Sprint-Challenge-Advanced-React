104be6778ba052d8c661277697af8ffdd38d6f40
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-22 11:51:28 -0800

    building out player data component

--- client/src/components/PlayerData.js
+++ client/src/components/PlayerData.js
@@ -5,17 +5,27 @@ import React from 'react';
 export default class PlayerData extends React.Component {
 
     state = {
-        loading: true
-    }
+        loading: true,
+        player: null
+    };
+
     async componentDidMount() {
         const url = 'http://localhost:5000/api/players';
         const response = await fetch(url);
         const data = await response.json();
+        this.setState({ player: data[0] });
         console.log(data);
     }
 
     render() {
-        return <div>{this.state.loading ? <div>loading...</div> : <div>player..</div>}</div>
+        return 
+            <div>
+                {this.state.loading || !this.state.player ? (
+                    <div>loading...</div>
+                ) : (
+                    <div>player..</div>
+                )}
+            </div>
     }
 
 }
