2531fbc9113474ea9748976dad696bfbdc507c6f
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-21 17:39:25 -0800

    wrote out dark mode hook using local storage to keep track of changes, need to change the css to make it work

---
+++ client/src/hooks/useDarkMode.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+
+    const toggleMode = e => {
+        e.prevent.default();
+        setDarkMode(!darkMode);
+    }
+    useEffect(() => {
+        if(darkMode === false) {
+            document.body.className = '';
+
+        }if(darkMode === true) {
+            document.body.className = 'dark-mode';
+        }
+    }, [darkMode], 
+    )
+
+    return [darkMode, toggleMode]
+}
