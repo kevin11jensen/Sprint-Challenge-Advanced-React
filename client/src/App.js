828b4a32c2503bc84cd8f4a61932beb6a96fea65
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-21 22:00:35 -0800

    got data to show, darkmode button doesn't work

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import axios from 'axios';
-
-
+import React, { useState } from 'react';
+import {Button} from 'reactstrap';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import useDarkMode from './hooks/useDarkMode';
 
 const App = () => {
-  
-  
-    return (
-      <div className="App">
-       <h1>am i even working?</h1>
-        </div>
-    )
-  
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <>
+    <div className="App">
+      <h1>Player List</h1>
+        <div className="navigation">
+          <Button className="dark-mode-toggle">
+            DarkMode
+            <div 
+            onClick={toggleMode}
+            className={darkMode ? 'toggle toggled' : 'toggle'}>
+
+            </div>
+        </Button>
+      </div>
+
+      <PlayerList />
+    </div>
+    </>
+  );
 }
-  
 
 export default App;
